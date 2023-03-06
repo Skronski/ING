4089f2d698e6aa392140edef6bb2fb1b503497f4
Kamil Skroński <kamil.skronski@gmail.com>
2023-03-06 23:23:25 +0100

    missing values imputation, removing outliers, de

--- script.R
+++ script.R
@@ -3,10 +3,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
-
 path <- 'D:\\Informatyka i ekonometria\\ING'
 setwd(path)
-
 
 
 # reading data ------------------------------------------------------------
@@ -17,55 +15,58 @@ summary(df)
 df$obs_date <-  as.Date(df$obs_date, format = "%Y-%m-%d")
 
 
-
-# missing data ------------------------------------------------------------
+# outliers, missing values ------------------------------------------------
 
 stat <- summary(df)
 
-nas <- as.data.frame(stat[7,])
+nas <- as.data.frame(stat[7, ])
 
 df_without_na <- na.omit(df)
-df_without_na_default <- df_without_na[df_without_na$default==1,]
+df_without_na_default <- df_without_na[df_without_na$default == 1, ]
+
+df_default <- df[df$default == 1, ]
+df_no_default <- df[df$default == 0, ]
+
+
+
+remove_outliers <- function(column)
+{
+  x <- quantile(column, na.rm = T)
+  names(x) <- NULL
+  IQR <- x[4] - x[2]
+  outlier_flag <- c(x[2] - IQR * 3, x[4] + IQR * 3)
+  column <- ifelse(column < outlier_flag[1], x[2], column)
+  column <- ifelse(column > outlier_flag[2], x[4], column)
+  return(column)
+}
+na_imputation <- function(column)
+{
+  column_mean <- mean(column, na.rm = T)
+  column <- ifelse(is.na(column), column_mean, column)
+}
+
+
+temp <- df[, !names(df) %in% c('ID', 'obs_date', 'default')]
+temp <-
+  as.data.frame(apply(temp, MARGIN = 2,  FUN = remove_outliers))
+temp <- as.data.frame(apply(temp, MARGIN = 2,  FUN = na_imputation))
+
 
 
 
 # visualization -----------------------------------------------------------
-df_viz <- df[,!names(df) %in% c('ID')]
+histogram(bins = 10)
 
-
-
-
-df_viz %>% ggplot(aes(x = df_viz$default)) +
-  geom_bar()+
-  xlab('default')
-
-x <- quantile(df_viz$Var_07)
-names(x) <- NULL
-IQR <- x[4]-x[2]
-outlier_flag <- c(x[2]-IQR*1.5, x[4]+IQR*1.5)
-
-df_viz$Var_07 <- ifelse(df_viz$Var_07<outlier_flag[1], x[2], df_viz$Var_07)
-
-df_viz$Var_07 <- ifelse(df_viz$Var_07>outlier_flag[2], x[4], df_viz$Var_07)
-
-
-
-df_viz %>% ggplot(aes(x = Var_07, y = default))+
-  geom_jitter()
-
-
-
-
-
-
+temp %>%
+  pivot_longer(cols = colnames(temp)) %>%
+  ggplot(aes(x = value)) +
+  geom_histogram() +
+  facet_wrap( ~ name, scales = 'free')
+temp %>%
+  pivot_longer(cols = colnames(temp)) %>%
+  ggplot(aes(y = value)) +
+  geom_violin(aes(x = name)) +
+  facet_wrap(~name, scales = 'free')
 
 
 # splitting data  ---------------------------------------------------------
-
-
-
-df_default <- df[df$default==1,]
-df_no_default <- df[df$default==0,]
-
-
-#test
